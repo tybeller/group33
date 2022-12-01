4436126968ba2517adffca71ca814492625d02e2
ejmercier <ejmercier15@gmail.com>
2022-12-01 13:22:30 -0500

    setup the navbar and page routing

---
+++ furever/frontend/src/pages/Dogs.js
@@ -0,0 +1,12 @@
+import '../index.css';
+function Dogs() {
+  return(
+  <div className="App bg-red-300">
+    <header classname="App-header">
+      Dogs page!!!
+    </header>
+  </div>
+  );
+}
+
+export default Dogs;
---
+++ furever/frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import '../index.css';
+function Home() {
+  return(
+  <div className="App bg-red-300">
+    <header classname="App-header">
+      Home page!!!
+    </header>
+  </div>
+  );
+}
+
+export default Home;
