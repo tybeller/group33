81e69debac07f9260e54e9fdfe7ec490ea304643
Kevin-Daniel <kevinadaniel7@gmail.com>
2022-12-06 22:20:20 -0500

    final changes

--- furever/frontend/src/pages/About.js
+++ furever/frontend/src/pages/About.js
@@ -1,10 +1,14 @@
 import '../index.css';
+import '../App.css';
 
+import AboutPage from '../components/AboutPage.js'
 function About() {
   return(
-  <div className="App bg-red-300">
+  <div className="App bg-blue-50">
     <header classname="App-header">
-      About page!!!
+      <div classname="aboutpage">
+          <AboutPage/>
+      </div>
     </header>
   </div>
   );
