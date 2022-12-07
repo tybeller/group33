08b0be7256f83accf40de92405a88b82b8e78f62
ejmercier <ejmercier15@gmail.com>
2022-12-06 21:21:10 -0500

    finished login pagegit status!

--- furever/frontend/src/App.js
+++ furever/frontend/src/App.js
@@ -2,8 +2,13 @@ import './index.css';
 import Header from './components/Header'
 import About from './pages/About';
 import Home from './pages/Home';
-import Users from './pages/Users';
 import Dogs from './pages/Dogs';
+
+import { UserProvider } from "./contexts/user.context";
+import Login from './pages/Login.page';
+import PrivateRoute from './pages/PrivateRoute.page';
+import Signup from './pages/Signup.page';
+import Logout from './pages/Logout';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Profile from './pages/Profile';
 
@@ -11,16 +16,23 @@ function App() {
   return (
     
     <BrowserRouter> 
-        <Header>
+      <Header> 
+        <UserProvider>
           <Routes>
-            <Route path='/about' element={<About />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/users' element={<Users />} />
-            <Route path='/dogs' element={<Dogs />} />
-            <Route path='/profile' element={<Profile />} />
-          </Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route element={<PrivateRoute />}> 
+              <Route path='/about' element={<About />} />
+              <Route path='/home' element={<Home />} />
+              <Route path='/dogs' element={<Dogs />} />
+              <Route path='/profile' element={<Profile />} />
+              <Route path='/logout' element={<Logout />} />
+            </Route>
             
-        </Header>
+          </Routes>
+        </UserProvider> 
+      </Header>
     </BrowserRouter>
        
   );
--- furever/frontend/src/pages/Home.js
+++ furever/frontend/src/pages/Home.js
@@ -1,5 +1,8 @@
 import '../index.css';
 import DogSwipe from '../components/DogSwipe'
+import { Button } from '@mui/material'
+import { useContext } from 'react';
+import { UserContext } from '../contexts/user.context';
 function Home() {
   return(
   <div className="App bg-red-300">
